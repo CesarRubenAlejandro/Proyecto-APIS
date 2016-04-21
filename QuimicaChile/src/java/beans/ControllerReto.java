ac4cf6ab31d7650469ca149d2f546e7329646eab
Cesar <cesars90_@hotmail.com>
2016-04-21 00:45:54 -0500

    Primera implementacion del drag and drop

--- QuimicaChile/src/java/beans/ControllerReto.java
+++ QuimicaChile/src/java/beans/ControllerReto.java
@@ -12,6 +12,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import org.primefaces.event.DragDropEvent;
 
 /**
  *
@@ -25,21 +26,18 @@ public class ControllerReto implements Serializable{
     private List<Chile> listaChiles;
     // lista de chiles que han sido movidos
     private List<Chile> dropedChiles;
-    // chile seleccionado
-    private Chile selectedChile;
     
     @PostConstruct
     public void init() {
         listaChiles = new ArrayList<Chile>();
         dropedChiles = new ArrayList<Chile>();
-        selectedChile = new Chile();
         
         // inicializar la lista de chiles
         Chile aux = new Chile("Chile Moruga","escorpion","2 millones de unidades de picor");
         listaChiles.add(aux);
         aux = new Chile("Habanero rojo de Sabina","habanero","500,000 mil a 1 millón de unidades de picor");
         listaChiles.add(aux);
-        aux = new Chile("Chile de árbol","chilearbol","15,000 a 30,000 unidades de picor");
+        aux = new Chile("Chile de arbol","chilearbol","15,000 a 30,000 unidades de picor");
         listaChiles.add(aux);
         aux = new Chile("Chile jalapeño, serrano, de árbol","jalapeno","5,000 a 25,000 unidades de picor");
         listaChiles.add(aux);
@@ -62,14 +60,13 @@ public class ControllerReto implements Serializable{
     public void setDropedChiles(List<Chile> dropedChiles) {
         this.dropedChiles = dropedChiles;
     }
-
-    public Chile getSelectedChile() {
-        return selectedChile;
-    }
-
-    public void setSelectedChile(Chile selectedChile) {
-        this.selectedChile = selectedChile;
-    }
     
-    
+    public void onChileDrop(DragDropEvent ddEvent) {
+        Chile aux = ((Chile) ddEvent.getData());
+        listaChiles.remove(aux);
+        dropedChiles.add(aux);
+        
+        System.out.println("ON DROP" + aux.getNombreChile());
+        
+    }
 }
